0f1bb48261381660a23705de5467f6d3410780c8
PalaceSwitcher <conmdelta@gmail.com>
2025-02-26 18:24:39 -0500

    Fix linking & add sprite view grid

--- src/UI/GfxView.cpp
+++ src/UI/GfxView.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <cmath>
 #include <iostream>
 
 std::vector<GfxAsset*> openedGfxFiles;
@@ -27,7 +28,6 @@ void renderSprites(GfxAsset* gfx, SDL_Renderer* ren) {
 		SDL_Texture* texture = SDL_CreateTextureFromSurface(ren, surf);
 		ImGui::Image((ImTextureID)(intptr_t)texture, ImVec2(sprite.width, sprite.height));
 	}
-	
 }
 
 void renderGfxWindow(SDL_Window* window, bool &opened) {
@@ -37,17 +37,34 @@ void renderGfxWindow(SDL_Window* window, bool &opened) {
 	int maxImgGridCols = 16;
 	int tabFlags = ImGuiTabBarFlags_Reorderable | ImGuiTabBarFlags_AutoSelectNewTabs | ImGuiTabBarFlags_TabListPopupButton | ImGuiTabBarFlags_DrawSelectedOverline;
 	if (ImGui::Begin("GFX View", &opened)) {
-		float windowWidth = ImGui::GetWindowSize().y;
-		int colCount = windowWidth / textureBoxSize;
+		float windowWidth = ImGui::GetWindowSize().x;
 		if (ImGui::BeginTabBar("GfxViewTabs", tabFlags)) {
 			for (auto file : openedGfxFiles) {
 				if (ImGui::BeginTabItem(file->name.c_str(), NULL, 0)) {
-					//for (int r = 0; r < 1; r++) {
-						//for (int c = 0; c < colCount; c++) {
-							//if (c > 0) { ImGui::SameLine(); }
-							renderSprites(file, ren);
-						//}
-					//}
+					int spriteCount = file->gfx.sprites[0].size();
+					int colCount = windowWidth / textureBoxSize; //Number of columns
+					if (colCount <= 0) { colCount = 1; }
+					int rowCount = spriteCount / colCount; //Number of rows
+					for (int sp = 0; sp < spriteCount; sp++) {
+						if (sp % colCount != 0) { ImGui::SameLine(); }
+						J2MEImage sprite = file->gfx.getSprite(0, sp);
+						std::vector<int> imgData = sprite.data;
+						SDL_Surface* surf = SDL_CreateSurfaceFrom(sprite.width, sprite.height, SDL_PIXELFORMAT_ARGB8888, imgData.data(), sprite.width*4);
+
+						SDL_Texture* texture = SDL_CreateTextureFromSurface(ren, surf);
+						SDL_SetTextureScaleMode(texture, SDL_SCALEMODE_NEAREST);
+						ImGui::PushID(sp);
+						ImGui::Selectable("##", true, 0, ImVec2(textureBoxSize, textureBoxSize));
+						ImGui::PopID();
+
+						float spriteDrawWidth = sprite.width;
+						float spriteDrawHeight = sprite.height;
+						ImVec2 min = ImGui::GetItemRectMax();
+						ImVec2 max = ImGui::GetItemRectMin();
+						ImVec2 center = ImVec2(min.x + (max.x - min.x - spriteDrawWidth) * 0.5f, min.y + (max.y - min.y - spriteDrawHeight) * 0.5f);
+						ImDrawList* draw_list = ImGui::GetWindowDrawList();
+						draw_list->AddImage((ImTextureID)(intptr_t)texture, center, ImVec2(center.x+spriteDrawWidth, center.y+spriteDrawHeight));
+					}
 					ImGui::EndTabItem();
 				}
 			}
